06f496d1a843888cf75c5cc3f97107c864707480
wrxue <a0981355215@gmail.com>
2020-11-03 08:22:57 +0800

    將日期部分以動態查詢，避免多出其他tr影響結果

--- check_out_mail.py
+++ check_out_mail.py
@@ -4,6 +4,7 @@ import time
 import smtplib
 from email.mime.text import MIMEText
 import json
+from datetime import datetime
 
 def send_email(subject, content):
         sender = json.load(open("./mailFrom.json", "r", encoding="utf-8"))
@@ -45,17 +46,22 @@ while 1:
     url = "https://pv.ncnu.edu.tw/EIP/Resource/Dashboard_ncnu.resource.aspx"
     response = sess.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    weekday = int(time.strftime("%w", time.localtime()))
-
-    today_tr = soup.find(id = "PersonalDuty_DataGrid").find_all("tr")[weekday + 2]
-    today_last_td = today_tr.find_all("td")[-1]
-
+    
+    today_last_td = None
+    for this_tr in soup.find(id = "PersonalDuty_DataGrid").find_all("tr"):
+        if(this_tr.find_all("td")[0].text == datetime.now().strftime("%Y-%m-%d")):
+            today_last_td = this_tr.find_all("td")[-1]
+            break
+        
+    if today_last_td is None:
+        break
     #get check-in record of the day
     url = "https://pv.ncnu.edu.tw/EIP/Resource/" + today_last_td.a["href"]
     response = sess.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     maybe_offwork = soup.find_all("tr")[-2].find_all("td")
     maybe_onwork = soup.find_all("tr")[4].find_all("td")
+    
     if maybe_onwork[1].text == "上班簽到":
         if maybe_offwork[1].text == "下班簽退":
             content = "!狂賀!!恭喜下班!!!" + "\n時間:" + maybe_offwork[2].text +"\n地點:" + maybe_offwork[-3].text
